a8863c63eaba30351cc7dbf62fe00576e460a445
klunkean <klunkean@users.noreply.github.com>
2019-12-12 12:09:28 +0100

    revert to master

--- pdfScraper.py
+++ pdfScraper.py
@@ -3,10 +3,6 @@ import glob
 import PyPDF2
 import textract
 
-from getpass import getpass
-import requests
-from bs4 import BeautifulSoup
-
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 
@@ -32,46 +28,6 @@ def extract_keywords(pdf_file):
 
 	return list(keywords)
 
-
-class FileFetcher(object):
-    def __init__(self):
-        #share url fuer plenumsprotokolle
-        self.base_url = "https://tubcloud.tu-berlin.de/s/60MBrwSi1LjYpFP/"
-
-    def _password_prompt(self):
-        passw = getpass(prompt="Passwort für die Datenbank eingeben: ")
-        return passw
-
-    def _login(self, session):
-        # establish cookie jar by logging in without doing anything
-        passw = self._password_prompt()
-        login_url = self.base_url + "authenticate/showShare"
-        first_response = session.get(login_url)
-        first_soup = BeautifulSoup(first_response.text, "html.parser")
-        request_token = first_soup.head.get("data-requesttoken")
-        sharing_token = first_soup.find("input", id="sharingToken").get("value")
-
-        post_data = {"password": passw}
-        post_data.update({"sharingToken": sharing_token})
-        post_data.update({"requesttoken": request_token})
-        
-        response = session.post(login_url, data=post_data)
-        soup = BeautifulSoup(response.text, "html.parser")
-        if not soup.find("div", {"class":"warning"}):
-            cookies = response.cookies
-        else:
-            raise ConnectionError("Verbindung konnte nicht hergestellt werden. Falsches Passwort?")
-        return cookies
-
-    def download_file(self, fname):
-        #TODO Raise Exception if fname doesn't exist in shared folder
-        with requests.Session() as session:
-            cookies = self._login(session)
-            download_url = self.base_url + "download?path=%2F&files=" + fname
-
-            byte_file = session.get(download_url, cookies = cookies, allow_redirects = True) 
-            with open(fname, "wb") as dlfile:
-                dlfile.write(byte_file.content)
 
 if __name__ == "__main__":
 
